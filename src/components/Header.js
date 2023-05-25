4a13b6df1d13d8e2ffd139510bbebacd7c7ce65b
Nkosinathi Tshaphile <tshaphilenkosinathi@gmail.com>
2023-05-25 23:26:30 +0200

    Finished working on the second header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,10 @@ import { BsSearch } from "react-icons/bs";
 const Header = () => {
   return (
     <>
-      <header className="header-top-strip py-3">
+      <header className="header-top-strip py-2">
         <div className="container-xxl">
           <div className="row">
-            <div className="col-6">
+            <div className="col-5">
               <p className="text-white mb-0">
                 This is not a real shopping site...yet.
               </p>
@@ -35,12 +35,12 @@ const Header = () => {
               <div className="input-group">
                 <input
                   type="text"
-                  className="form-control py-2"
+                  className="form-control py-1"
                   placeholder="Search Product Here..."
                   aria-label="Search Product Here..."
                   aria-describedby="basic-addon2"
                 />
-                <span className="input-group-text p-3" id="basic-addon2">
+                <span className="input-group-text p-2" id="basic-addon2">
                   <BsSearch className="fs-6" />
                 </span>
               </div>
@@ -48,36 +48,51 @@ const Header = () => {
             <div className="col-5">
               <div className="header-upper-links d-flex align-items-center justify-content-between">
                 <div>
-                  <Link>
+                  <Link className="d-flex align-items-center gap-10 text-white">
                     <img src="/images/compare.svg" alt="compare"></img>
-                    <p>
+                    <p className="mb-0">
                       Compare <br /> Products
                     </p>
                   </Link>
                 </div>
                 <div>
-                  <Link>
+                  <Link className="d-flex align-items-center gap-10 text-white">
                     <img src="/images/wishlist.svg" alt="wishlist"></img>
-                    <p>
+                    <p className="mb-0">
                       Favorite <br /> wishlist
                     </p>
                   </Link>
                 </div>
                 <div>
-                  <Link>
+                  <Link className="d-flex align-items-center gap-10 text-white">
                     <img src="/images/user.svg" alt="user"></img>
-                    <p>
+                    <p className="mb-0">
                       Log in <br /> My Account
                     </p>
                   </Link>
                 </div>
                 <div>
-                  <Link>
+                  <Link className="d-flex align-items-center gap-10 text-white">
                     <img src="/images/cart.svg" alt="cart"></img>
-                    <div className="d-flex flex-column">
+                    <div className="d-flex flex-column gap-10">
                       <span className="badge bg-white text-dark">0</span>
+                      <p className="mb-0">R 500.00</p>
                     </div>
                   </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="header-bottom py-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="menu-bottom d-flex align-items-center">
+                <div></div>
+                <div className="menu-links">
+                  <div className="d-flex"></div>
                 </div>
               </div>
             </div>
