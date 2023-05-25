ec28a64f2635e9fb8e95cdf1cd26093f4dcde416
Nkosinathi Tshaphile <tshaphilenkosinathi@gmail.com>
2023-05-25 20:54:51 +0200

    Added images.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navlink, Link } from "react-router-dom";
+import { BsSearch } from "react-icons/bs";
 const Header = () => {
   return (
     <>
@@ -8,7 +9,7 @@ const Header = () => {
           <div className="row">
             <div className="col-6">
               <p className="text-white mb-0">
-                Free Shipping Over $100 & Free Returns
+                This is not a real shopping site...yet.
               </p>
             </div>
             <div className="col-6">
@@ -24,14 +25,56 @@ const Header = () => {
       </header>
       <header className="header-upper py-3">
         <div className="container-xxl">
-          <div className="row">
+          <div className="row align-items-center">
             <div className="col-2">
               <h2>
-                <Link className="text-white ">Nathi Tech</Link>
+                <Link className="text-white">Nathi Tech</Link>
               </h2>
             </div>
-            <div className="col-5"></div>
-            <div className="col-5"></div>
+            <div className="col-5">
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control py-2"
+                  placeholder="Search Product Here..."
+                  aria-label="Search Product Here..."
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text p-3" id="basic-addon2">
+                  <BsSearch className="fs-6" />
+                </span>
+              </div>
+            </div>
+            <div className="col-5">
+              <div className="header-upper-links d-flex align-items-center justify-content-between">
+                <div>
+                  <Link>
+                    <img src="/images/compare.svg" alt=""></img>
+                    <p>
+                      Compare <br /> Products
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link>
+                    <img src="" alt=""></img>
+                    <p></p>
+                  </Link>
+                </div>
+                <div>
+                  <Link>
+                    <img src="" alt=""></img>
+                    <p></p>
+                  </Link>
+                </div>
+                <div>
+                  <Link>
+                    <img src="" alt=""></img>
+                    <p></p>
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </header>
