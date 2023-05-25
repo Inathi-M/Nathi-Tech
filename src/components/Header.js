3e030b782e6b6f939b929df523b50fadb742ef7c
Nkosinathi Tshaphile <tshaphilenkosinathi@gmail.com>
2023-05-25 20:01:06 +0200

    Integrated the CSS dependancies

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,42 @@
 import React from "react";
-
+import { Navlink, Link } from "react-router-dom";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <header className="header-top-strip py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <p className="text-white mb-0">
+                Free Shipping Over $100 & Free Returns
+              </p>
+            </div>
+            <div className="col-6">
+              <p className="text-end text-white mb-0">
+                Hotline:
+                <a className="text-white" href="tel:+27 620338048">
+                  +27 62 033 8048
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="header-upper py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-2">
+              <h2>
+                <Link className="text-white ">Nathi Tech</Link>
+              </h2>
+            </div>
+            <div className="col-5"></div>
+            <div className="col-5"></div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Header;
