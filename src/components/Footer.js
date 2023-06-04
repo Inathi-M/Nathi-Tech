409e7545fee9e5ce99a83b6064013811e456514c
Nkosinathi Tshaphile <tshaphilenkosinathi@gmail.com>
2023-06-04 23:09:15 +0200

    Fixed all the errors I had.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { BsLinkedin, BsGithub, BsYoutube } from "react-icons/bs";
 const Footer = () => {
   return (
     <>
       <footer className="py-4">
         <div className="container-xxl">
           <div className="row align-items-center">
-            <div className="col-5">
-              <div className="footer-top-data d-flex gap-30 align-items-center">
+            <div className="col-6">
+              <div className="footer-top-data d-flex gap-15 align-items-center">
                 <img src="images/newsletter.png" alt="newsletter" />
                 <h2 className="mb-0 text-white">Sign Up for Newsletter</h2>
               </div>
             </div>
             <div className="col-5">
-              <div className="input-group">
+              <div className="input-group fs-5">
                 <input
                   type="text"
                   className="form-control py-1"
@@ -34,19 +35,63 @@ const Footer = () => {
           <div className="row">
             <div className="col-4">
               <h4 className="text-white mb-4">Contact Me</h4>
-              <div></div>
+              <div className="footer-links d-flex flex-column">
+                <address className="text-white fs-7">
+                  BT 643 Vukani Street, Site C, Khayelitsha, <br /> Cape Town
+                  <br />
+                  7784
+                </address>
+                <a
+                  href="tel:+27 620338048"
+                  className="mt-4 d-block mb-2 text-white"
+                >
+                  +27 62 0338048
+                </a>
+                <a
+                  href="mailto:tshaphilenkosinathi@gmail.com"
+                  className="mt-4 d-block mb-2 text-white"
+                >
+                  tshaphilenkosinathi@gmail.com
+                </a>
+                <div className="social_icons">
+                  <a href="">
+                    <BsLinkedin />
+                  </a>
+                  <a href="">
+                    <BsGithub />
+                  </a>
+                  <a href="">
+                    <BsYoutube />
+                  </a>
+                </div>
+              </div>
             </div>
             <div className="col-3">
               <h4 className="text-white mb-4">Information</h4>
-              <div></div>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white py-2 mb-1">Privacy Policy</Link>
+                <Link className="text-white py-2 mb-1">Refund Policy</Link>
+                <Link className="text-white py-2 mb-1">Shipping Policy</Link>
+                <Link className="text-white py-2 mb-1">Terms & Conditions</Link>
+                <Link className="text-white py-2 mb-1">Blogs</Link>
+              </div>
             </div>
             <div className="col-3">
               <h4 className="text-white mb-4">Account</h4>
-              <div></div>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white py-2 mb-1">About Us</Link>
+                <Link className="text-white py-2 mb-1">FAQ</Link>
+                <Link className="text-white py-2 mb-1">Contact</Link>
+              </div>
             </div>
             <div className="col-2">
               <h4 className="text-white mb-4">Quick Links</h4>
-              <div className="footer-links"></div>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white py-2 mb-1">Laptops</Link>
+                <Link className="text-white py-2 mb-1">Headphones</Link>
+                <Link className="text-white py-2 mb-1">Tablets</Link>
+                <Link className="text-white py-2 mb-1">Watches</Link>
+              </div>
             </div>
           </div>
         </div>
