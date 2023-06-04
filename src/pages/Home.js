fb2ba4ab822b7c41ce46eef007d4ab29638e7406
Nkosinathi Tshaphile <tshaphilenkosinathi@gmail.com>
2023-06-05 00:26:00 +0200

    Adding styling to the banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <>
@@ -7,12 +7,18 @@ const Home = () => {
         <div className="container-xxl">
           <div className="row">
             <div className="col-6">
-              <div className="main-banner-content p-3">
+              <div className="main-banner position-relative  p-3">
                 <img
                   src="images/main-banner-1.jpg"
                   className="img-fluid rounded-3"
                   alt="main banner"
                 />
+                <div className="main-banner-content position-absolute ">
+                  <h4>SUPERCHARGED FOR PROS.</h4>
+                  <h5>iPad S13+ Pro.</h5>
+                  <p>From R15 000 or R800/mo.</p>
+                  <Link>BUY NOW</Link>
+                </div>
               </div>
             </div>
             <div className="col-6"></div>
