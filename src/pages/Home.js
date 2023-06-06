f2bce160f67c1ba721f11fde08a2d9618d95e71c
Nkosinathi Tshaphile <tshaphilenkosinathi@gmail.com>
2023-06-06 21:14:28 +0200

    Adding second banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,11 +17,27 @@ const Home = () => {
                   <h4>SUPERCHARGED FOR PROS.</h4>
                   <h5>iPad S13+ Pro.</h5>
                   <p>From R15 000 or R800/mo.</p>
-                  <Link>BUY NOW</Link>
+                  <Link className="button">BUY NOW</Link>
                 </div>
               </div>
             </div>
-            <div className="col-6"></div>
+            <div className="col-6">
+              <div className="d-flex flex-wrap justify-content-between align-items-center">
+                <div className="small-banner position-relative  p-3">
+                  <img
+                    src="images/catbanner-01.jpg"
+                    className="img-fluid rounded-3"
+                    alt="main banner"
+                  />
+                  <div className="small-banner-content position-absolute ">
+                    <h4>SUPERCHARGED FOR PROS.</h4>
+                    <h5>iPad S13+ Pro.</h5>
+                    <p>From R15 000 or R800/mo.</p>
+                    <Link className="button">BUY NOW</Link>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
