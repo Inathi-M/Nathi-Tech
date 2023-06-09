59e0853bd1e6ea1e9376ece1dfae17072765006b
Nkosinathi Tshaphile <tshaphilenkosinathi@gmail.com>
2023-06-09 12:35:12 +0200

    This is my last commit before pushing to git.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -236,6 +236,9 @@ const Home = () => {
       <div className="section blog-wrapper py-5 home-wrapper-2">
         <div className="container-xxl">
           <div className="row">
+            <div className="col-12">
+              <h3 className="section-heading">My Latest Blogs</h3>
+            </div>
             <BlogCards />
             <BlogCards />
             <BlogCards />
